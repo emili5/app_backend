d666aefa20aa1951205728ac14ca12abbefe496a
emili5 <88500087+emili5@users.noreply.github.com>
2022-02-08 17:51:57 +0900

    Add files via upload

---
+++ app_backend/backend/src/main/java/com/harustation/backend/service/OrderService.java
@@ -0,0 +1,17 @@
+package com.harustation.backend.service;
+
+import com.harustation.backend.repository.OrderRepository;
+import com.harustation.backend.repository.ProductRepository;
+import com.harustation.backend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    private ProductRepository productRepository;
+    private OrderRepository orderRepository;
+    private UserRepository userRepository;
+
+    //주문
+
+}
